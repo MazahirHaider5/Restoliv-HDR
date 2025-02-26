5454633fbcfd55047fb74725b296d8b1602a8830
CH.Fayyaz <chfayyaz246@gmail.com>
2025-02-26 15:53:09 +0500

    fixed error

--- src/firebase.js
+++ src/firebase.js
@@ -1,65 +1,107 @@
-import { initializeApp, getApps, getApp } from 'firebase/app'
-import {
-    getMessaging,
-    getToken,
-    onMessage,
-    isSupported,
-} from 'firebase/messaging'
-import { useStoreFcm } from './hooks/react-query/push-notification/usePushNotification'
-const firebaseConfig = {
-    apiKey: "AIzaSyBhjyeVEayrX8NZBGOWVjUlaSAf5k1rop0",
-    authDomain: "restoliv-7c16f.firebaseapp.com",
-    projectId: "restoliv-7c16f",
-    storageBucket: "restoliv-7c16f.firebasestorage.app",
-    messagingSenderId: "1033834088012",
-    appId: "1:1033834088012:web:276a0112246b51bd4656e9",
-    measurementId: "G-ZLPPL4DVEV"
-  };
+import React, { useEffect, useState } from 'react'
+import RestaurantDetails from '../../../components/restaurant-details/RestaurantDetails'
+import Meta from '../../../components/Meta'
+import MainApi from '../../../api/MainApi'
+import { useDispatch, useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+import { CustomHeader } from '../../../api/Headers'
+import { setGlobalSettings } from "@/redux/slices/global"
 
-const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const messaging = (async () => {
-    try {
-        const isSupportedBrowser = await isSupported()
-        if (isSupportedBrowser) {
-            return getMessaging(firebaseApp)
-        }
+const index = ({ restaurantData, configData }) => {
+    const { global } = useSelector((state) => state.globalSettings)
+    const restaurantCoverUrl = global?.base_urls?.restaurant_cover_photo_url
+    const restaurantCoverPhoto = `${restaurantCoverUrl}/${restaurantData?.cover_photo}`
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const { restaurant_zone_id } = router.query
 
-        return null
-    } catch (err) {
-        return null
-    }
-})()
+    const [zoneId, setZoneId] = useState(undefined)
 
-export const fetchToken = async (setTokenFound, setFcmToken) => {
-    return getToken(await messaging, {
-        vapidKey:
-            'BBEVtGI3ZXlRQH3CS1nANY8Cq6c209sXx5Hi5iBZGZP2tP0fhLfpKR6ScBYIK01kJfRHdxZyqm6BSG_U6XjK7y4',
-    })
-        .then((currentToken) => {
-            if (currentToken) {
-                setTokenFound(true)
-                setFcmToken(currentToken)
-
-                // Track the token -> client mapping, by sending to backend server
-                // show on the UI that permission is secured
-            } else {
-                setTokenFound(false)
-                setFcmToken()
-                // shows on the UI that permission is required
+    // Get zoneId from localStorage
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            try {
+                const storedZoneId = localStorage.getItem('zoneid');
+                setZoneId(storedZoneId);
+            } catch (error) {
+                console.error('LocalStorage error:', error);
             }
-        })
-        .catch((err) => {
-            console.error(err)
-            // catch error while creating client token
-        })
+        }
+    }, []);
+
+    // Set global settings
+    useEffect(() => {
+        if (configData) {
+            dispatch(setGlobalSettings(configData))
+        }
+    }, [dispatch, configData]);
+
+    // Handle maintenance mode
+    useEffect(() => {
+        if (configData?.maintenance_mode) {
+            router.push('/maintenance');
+        }
+    }, [configData, router]);
+
+    // Save zoneId in localStorage
+    useEffect(() => {
+        if (restaurant_zone_id && !zoneId) {
+            localStorage.setItem('zoneid', JSON.stringify([Number(restaurant_zone_id)]))
+        }
+    }, [restaurant_zone_id, zoneId])
+
+    // Handle missing data
+    if (!restaurantData || !configData) return <div>Loading...</div>
+
+    return (
+        <>
+            <Meta
+                title={`${restaurantData?.meta_title ?? restaurantData?.name} - ${configData?.business_name}`}
+                ogImage={`${configData?.base_urls?.restaurant_image_url}/${restaurantData?.meta_image}`}
+                description={restaurantData?.meta_description}
+            />
+            <RestaurantDetails restaurantData={restaurantData} />
+        </>
+    )
 }
 
-export const onMessageListener = async () =>
-    new Promise((resolve) =>
-        (async () => {
-            const messagingResolve = await messaging
-            onMessage(messagingResolve, (payload) => {
-                resolve(payload)
-            })
-        })()
-    )
+export default index
+
+// Fetch restaurant data on server-side
+export const getServerSideProps = async (context) => {
+    const id = context.query.id
+    const { req } = context
+    const language = req.cookies.languageSetting
+
+    try {
+        const data = await MainApi.get(`/api/v1/restaurants/details/${id}`)
+        const configRes = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/config`,
+            {
+                method: 'GET',
+                headers: {
+                    'X-software-id': 33571750,
+                    'X-server': 'server',
+                    'X-localization': language,
+                    origin: process.env.NEXT_CLIENT_HOST_URL,
+                },
+            }
+        )
+        const config = await configRes.json()
+
+        return {
+            props: {
+                restaurantData: data.data,
+                configData: config,
+            },
+        }
+    } catch (error) {
+        console.error("Data fetch error:", error)
+        return {
+            props: {
+                restaurantData: null,
+                configData: null,
+            },
+        }
+    }
+}
